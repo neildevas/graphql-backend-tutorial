92d708be88e2d1e782c95894d8cdc3780831b5ff
Neil Devas <neil.devas97@gmail.com>
2020-10-16 20:50:18 -0400

    Update schema and add resolvers

--- src/index.js
+++ src/index.js
@@ -5,43 +5,85 @@ const { GraphQLServer } = require('graphql-yoga');
 // SDL allows you to define types
 // Three root types: Query, Mutation, Subscription
 // Each root type can take fields (called root fields) which will define the available API operations
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-  
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
 
-const links = [{
+let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
+  description: 'Fullstack tutorial for GraphQL',
+  is_deleted: false,
 }, {
   id: 'foo',
   description: 'bar',
   url: 'baz.com',
+  is_deleted: false,
 }];
+
+let getIdCount = () => links.length;
+const createPost = (parent, args) => {
+  console.log('PARENT', parent);
+  console.log('ARGS', args);
+  const newPost = {
+    id: `id-${getIdCount()}`,
+    description: args.description,
+    url: args.url,
+  };
+  links.push(newPost);
+  return newPost;
+};
+
+const findLink = (id) => {
+  const link = links.find(link => link.id === id);
+  if (!link) {
+    throw new Error('Could not find link');
+  }
+  return link
+};
+
+const findLinkById = (parent, args) => {
+  const { id } = args;
+  return links.find(link => link.id === id);
+};
+
+const updateLink = (parent, args) => {
+  const { linkId, description, url } = args;
+  const link = findLink(linkId);
+  if (description) {
+    link.description = description;
+  }
+  if (url) {
+    link.url = url;
+  }
+  return link
+};
+
+const deleteLink = (parent, args) => {
+  const { linkId } = args;
+  const linkToDelete = findLink(linkId);
+  linkToDelete.is_deleted = true;
+  links = links.map(link => {
+    if (link.id === linkToDelete.id) {
+      return linkToDelete;
+    }
+    return link;
+  });
+  return linkToDelete
+};
 
 const resolvers = { // functions that get data
   Query: {
     info: () => `Api for hackernews`,
-    feed: () => links
+    feed: () => links,
+    link: findLinkById,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url,
-  }
+  Mutation: {
+    createPost,
+    updateLink,
+    deleteLink
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log('Server is running on port 4000'));
