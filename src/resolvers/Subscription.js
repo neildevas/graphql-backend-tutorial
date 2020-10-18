f16f711f21daa9d48aef3106b0859d6f1081ecfc
Neil Devas <neil.devas97@gmail.com>
2020-10-18 18:08:58 -0400

    Add new vote subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -3,11 +3,21 @@ function newLinkSubscribe(parent, args, context, info) {
   return context.pubsub.asyncIterator('NEW_LINK');
 }
 
+function newVoteSubscribe(parent, args, context) {
+  return context.pubsub.asyncIterator("NEW_VOTE")
+}
+
 const newLink = {
   subscribe: newLinkSubscribe,
   resolve: payload => payload, // returns the value returned by the resolver
 };
 
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => payload,
+};
+
 module.exports = {
   newLink,
+  newVote,
 };
